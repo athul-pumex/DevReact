9c6fa8c80152131c433cdfed8c217e07f1181db8
athul4040 <athul4040@gmail.com>
2020-05-22 19:33:49 +0530

    API call configuration

--- myproject/src/components/login.component.js
+++ myproject/src/components/login.component.js
@@ -1,20 +1,51 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { PostData } from "../services/PostData"
 
 export default class Login extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            email: '',
+            password: ''
+        }
+        this.login = this.login.bind(this)
+        this.onChange = this.onChange.bind(this)
+
+    }
+
+    //login function called on form submit
+    login() {
+        // event.preventDefault();
+        // event.stopPropagation();
+        console.log("hi login works")
+        PostData('getUserByEmailAndPassword', this.state).then((result) => {
+            let responseJSON = result;
+            console.log(responseJSON);
+        })
+        console.log("hello")
+
+    }
+    onChange(e) {
+
+        this.setState({ [e.target.name]: e.target.value });
+
+    }
+
     render() {
         return (
-            <form>
+            <form onSubmit={this.login}>
                 <h3>Sign In</h3>
 
                 <div className="form-group">
                     <label>Email address</label>
-                    <input type="email" className="form-control" placeholder="Enter email" />
+                    <input name="email" type="email" className="form-control" placeholder="Enter email" required onChange={this.onChange} />
                 </div>
 
                 <div className="form-group">
                     <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password" />
+                    <input name="password" type="password" className="form-control" placeholder="Enter password" required title="Please enter the Password" onChange={this.onChange} />
                 </div>
 
                 <div className="form-group">
@@ -24,7 +55,7 @@ export default class Login extends Component {
                     </div>
                 </div>
 
-                <button type="submit" className="btn btn-primary btn-block">Submit</button>
+                <button type="submit" className="btn btn-primary btn-block" >Submit</button>
                 <p className="forgot-password text-right">
                     <Link className="nav-link" to={"/forgotPassword"}> Forgot password?</Link>
                 </p>
--- myproject/src/components/updatePassword.component.js
+++ myproject/src/components/updatePassword.component.js
@@ -6,7 +6,7 @@ export default class UpdatePassword extends Component {
             <form> <h3>Reset your password</h3><br></br>
                 <div className="form-group">
                     <label>New Password</label>
-                    <input type="password" className="form-control" placeholder="Enter new password" /><br></br>
+                    <input type="password" className="form-control" placeholder="Enter new password" required="true" /><br></br>
                     <label>Confirm Password</label>
                     <input type="password" className="form-control" placeholder="Confirm new password" /><br></br>
                     <button type="submit" className="btn btn-primary btn-block">Reset Password</button>
